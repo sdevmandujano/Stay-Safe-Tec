18bbb5098421f37fa518204182188394cc46ea37
sdevmandujano <zmandujano01@gmail.com>
2018-09-21 18:01:33 -0500

    adding files

---
+++ app.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    $("#picTwitter").click(function () {
+        $(".detailsCard").toggle("slow", function () {
+
+        });
+    });
+
+
+    $("#picDanger").click(function () {
+        $(".detailsCard2").toggle("slow", function () {
+
+        });
+    });
+
+});
